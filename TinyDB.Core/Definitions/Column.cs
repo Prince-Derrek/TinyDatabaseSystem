aabe657359bafbdebad5ede3f60f7ccaad559960
Derrek Kimani <derrekkimani@proton.me>
2026-01-14 09:55:32 +0300

    feat(data-model): Created the definition for Column headers

---
+++ TinyDB.Core/Definitions/Column.cs
@@ -0,0 +1,14 @@
+﻿namespace TinyDB.Core.Definitions
+{
+    public class Column
+    {
+        public string Name { get; }
+        public ColumnType Type { get; }
+
+        public Column(string name, ColumnType type)
+        {
+            Name = name;
+            Type = type;
+        }
+    }
+}
