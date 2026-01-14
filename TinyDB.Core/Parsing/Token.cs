10967e2c0b04b429c764aff4ddcd95ee60f49cec
Derrek Kimani <derrekkimani@proton.me>
2026-01-14 10:11:24 +0300

    feat(DSL): Created a class to hod the token data

---
+++ TinyDB.Core/Parsing/Token.cs
@@ -0,0 +1,21 @@
+﻿namespace TinyDB.Core.Parsing
+{
+    public class Token
+    {
+        public TokenType Type { get; }
+        public string Value { get; }
+        public int Position { get; } // Good for error reporting (e.g. "Error at char 5")
+
+        public Token(TokenType type, string value, int position)
+        {
+            Type = type;
+            Value = value;
+            Position = position;
+        }
+
+        public override string ToString()
+        {
+            return $"Token({Type}, '{Value}')";
+        }
+    }
+}
