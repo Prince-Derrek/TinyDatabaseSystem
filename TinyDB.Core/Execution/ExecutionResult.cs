26087b49d3767cb6aac78c3c8fb8779e31893076
Derrek Kimani <derrekkimani@proton.me>
2026-01-14 10:20:22 +0300

    feat(excution): Created an object to represent what happened after a query runs

---
+++ TinyDB.Core/Execution/ExecutionResult.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+
+namespace TinyDB.Core.Execution
+{
+    public class ExecutionResult
+    {
+        public string Message { get; }
+        public List<string> Columns { get; }
+        public List<object[]> Rows { get; }
+        public bool IsQuery { get; } // True for SELECT, False for INSERT/CREATE
+
+        // Constructor for commands (CREATE, INSERT)
+        public ExecutionResult(string message)
+        {
+            Message = message;
+            Columns = new List<string>();
+            Rows = new List<object[]>();
+            IsQuery = false;
+        }
+
+        // Constructor for queries (SELECT)
+        public ExecutionResult(List<string> columns, List<object[]> rows)
+        {
+            Message = $"Returned {rows.Count} rows.";
+            Columns = columns;
+            Rows = rows;
+            IsQuery = true;
+        }
+    }
+}
