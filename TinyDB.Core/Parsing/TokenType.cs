d0949b7ac83e1e3d071d3b12dcd0cb3d237f8324
Derrek Kimani <derrekkimani@proton.me>
2026-01-14 10:09:59 +0300

    feat(DSL): Created parsing for token type whch defines every atom our language understands

---
+++ TinyDB.Core/Parsing/TokenType.cs
@@ -0,0 +1,38 @@
+﻿namespace TinyDB.Core.Parsing
+{
+    public enum TokenType
+    {
+        // Keywords
+        CREATE,
+        TABLE,
+        INSERT,
+        INTO,
+        VALUES,
+        SELECT,
+        FROM,
+        WHERE,   // Reserved for later
+
+        // Data Types
+        INT_TYPE,
+        STRING_TYPE,
+        BOOL_TYPE,
+
+        // Symbols / Punctuation
+        OPEN_PAREN,   // (
+        CLOSE_PAREN,  // )
+        COMMA,        // ,
+        STAR,         // * (Wildcard)
+        SEMICOLON,    // ;
+
+        // Literals (The actual data)
+        INTEGER_LITERAL, // 123
+        STRING_LITERAL,  // 'Derrek'
+        BOOLEAN_LITERAL, // true/false
+
+        // Identifiers
+        IDENTIFIER,      // Table or Column names (e.g., Users, Id)
+
+        // End of File
+        EOF
+    }
+}
